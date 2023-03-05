bb81fd64e7159eec3c1f1b0fd0484ce99fe72b88
YoshinobuKataoka <yoshi37770129@au.com>
2023-03-05 17:46:37 +0900

    EntityScene_01のExecuteAuthoringを作成

---
+++ Assets/Main/Executes/EntityScene_01/ExecuteAuthoring.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace UnityEcsTest.Main.Executes.EntityScene_01
+{
+    public class ExecuteAuthoring : MonoBehaviour
+    {
+        class Baker : Baker<ExecuteAuthoring>
+        {
+            public override void Bake(ExecuteAuthoring authoring)
+            {
+                AddComponent<Execute>();
+            }
+        }
+    }
+
+    public struct Execute : IComponentData
+    {
+    }
+}
